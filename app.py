9d48009697332b613369b8ca89769061c745d944
nivedhasvlr-collab <nivedhasvlr@gmail.com>
2026-09-17 11:52:30 +0530

    Add Project 2 files

---
+++ app.py
@@ -0,0 +1,5 @@
+with open("report.txt", "w") as f:
+    f.write("Student Academic Performance System Report\n")
+    f.write("Total Enrolled Users: 120\n")
+    f.write("Active Student Sessions: 45\n")
+print("Report generated successfully.")
